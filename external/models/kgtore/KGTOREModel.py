63b306035ada23f4dadbc9cd8fef45906d3752ed
dmalitesta <d.malitesta@gmail.com>
2023-01-10 10:05:20 +0100

    KGTORE config added for ablation study.

--- external/models/kgtore/KGTOREModel.py
+++ external/models/kgtore/KGTOREModel.py
@@ -75,6 +75,8 @@ class KGTOREModel(torch.nn.Module, ABC):
         self.optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
 
     def propagate_embeddings(self, evaluate=False):
+        print(self.edge_features.device)
+        print(self.F.device)
         edge_embeddings = matmul(self.edge_features, self.F)
         nonzero = torch.count_nonzero(self.edge_features.to_dense(), dim=1)
         edge_embeddings = torch.div(edge_embeddings, nonzero.reshape(-1, 1))
