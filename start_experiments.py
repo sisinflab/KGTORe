f6d95649ce41891602cb2eb0bfc22f7784bce733
dmalitesta <d.malitesta@gmail.com>
2023-01-10 09:48:58 +0100

    KGTORE config added for ablation study.

--- start_experiments.py
+++ start_experiments.py
@@ -2,7 +2,7 @@ from elliot.run import run_experiment
 import argparse
 
 parser = argparse.ArgumentParser(description="Run sample main.")
-parser.add_argument('--config', type=str, default='kgat_movielens')
+parser.add_argument('--config', type=str, default='kgtore_movielens')
 args = parser.parse_args()
 
 run_experiment(f"config_files/{args.config}.yml")
---
+++ start_experiments_kgtore.py
@@ -0,0 +1,15 @@
+from elliot.run import run_experiment
+import argparse
+
+parser = argparse.ArgumentParser(description="Run sample main.")
+parser.add_argument('--dataset', type=str, default='movielens')
+args = parser.parse_args()
+
+npr = [1, 3, 10]
+criterion = ['entropy', 'gini']
+
+for n in npr:
+    for c in criterion:
+        print(f'Starting training with npr: {n}, criterion: {c}')
+        run_experiment(f"config_files/kgtore_{args.dataset}_{n}_{c}.yml")
+        print('End training with npr: {n}, criterion: {c}')
