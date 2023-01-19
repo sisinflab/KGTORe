3b4389d5aadd252eabf02650a0c162fa165528cf
albertomancino <alberto.mancino94@gmail.com>
2023-01-19 20:06:26 +0100

    experimental setting

--- external/models/kgtore/KGTOREModel.py
+++ external/models/kgtore/KGTOREModel.py
@@ -93,7 +93,7 @@ class KGTOREModel(torch.nn.Module, ABC):
 
     def propagate_embeddings(self, evaluate=False):
 
-        edge_embeddings_u_i = matmul(self.edge_features, self.F.to(self.device)) * (1 - self.b)
+        edge_embeddings_u_i = matmul(self.edge_features, self.F.to(self.device))# * (1 - self.b)
         edge_embeddings_i_u = matmul(self.item_features, self.F.to(self.device))[self.items] * (1-self.a)
 
         ego_embeddings = torch.cat((self.Gu.to(self.device), self.Gi.to(self.device)), 0)
