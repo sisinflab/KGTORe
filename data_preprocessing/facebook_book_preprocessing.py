58dc27fc6682a188f5023e10f447d786c8110fd5
albertomancino <alberto.mancino94@gmail.com>
2023-01-19 19:27:12 +0100

    experimental setting

--- data_preprocessing/facebook_book_preprocessing.py
+++ data_preprocessing/facebook_book_preprocessing.py
@@ -63,10 +63,6 @@ def run(data_folder):
                                           folder=os.path.join(data_folder, 'kgflex'),
                                           name='kg',
                                           message='knowledge graph')
-        store_dataset(kgflex._kwargs['item_features'],
-                      folder=os.path.join(data_folder, 'kgflex'),
-                      name='item_features',
-                      message='item features')
         del kgflex
 
         kahfm = KaHFMFilter(**kwargs)
