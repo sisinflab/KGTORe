8d968ca39c42c59aea1dd5b177edec2ca1143550
alberto <alberto.mancino94@gmail.com>
2023-01-23 19:33:02 +0100

    kgin test

--- elliot/recommender/knowledge_aware/kgin/kgin.py
+++ elliot/recommender/knowledge_aware/kgin/kgin.py
@@ -71,8 +71,8 @@ class KGIN(RecMixin, BaseRecommenderModel):
             # D^{-1/2}AD^{-1/2}
             rowsum = np.array(adj.sum(1))
 
-            d_inv_sqrt = np.power(rowsum, -0.5).flatten()
-            d_inv_sqrt[np.isinf(d_inv_sqrt)] = 0.
+            d_inv_sqrt = np.power(rowsum, -0.5, where=(rowsum != 0)).flatten()
+            # d_inv_sqrt[np.isinf(d_inv_sqrt)] = 0.
             d_mat_inv_sqrt = sp.diags(d_inv_sqrt)
 
             # bi_lap = adj.dot(d_mat_inv_sqrt).transpose().dot(d_mat_inv_sqrt)
@@ -83,8 +83,8 @@ class KGIN(RecMixin, BaseRecommenderModel):
             # D^{-1}A
             rowsum = np.array(adj.sum(1))
 
-            d_inv = np.power(rowsum, -1).flatten()
-            d_inv[np.isinf(d_inv)] = 0.
+            d_inv = np.power(rowsum, -1, where=(rowsum != 0)).flatten()
+            # d_inv[np.isinf(d_inv)] = 0.
             d_mat_inv = sp.diags(d_inv)
 
             norm_adj = d_mat_inv.dot(adj)
