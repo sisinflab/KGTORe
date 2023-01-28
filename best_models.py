097704fddc348b2e37567514085d33dfbe8613f2
albertomancino <alberto.mancino94@gmail.com>
2023-01-28 22:22:55 +0100

    yahoo best

--- best_models.py
+++ best_models.py
@@ -3,20 +3,20 @@ from elliot.run import run_experiment
 
 CONFIG_DIR = './config_files'
 
-facebook_best_models = ['facebook_best_models',
+facebook_best_models = ['facebook_noknowledge_best',
                         'facebook_kahfm_best',
                         'facebook_kgflex_best',
                         'facebook_kgin_best',
                         'facebook_kgtore_best']
 
-yahoo_best_models = ['yahoo_best_models',
+yahoo_best_models = ['yahoo_noknowledge_best',
                      'yahoo_kahfm_best',
                      'yahoo_kgflex_best',
                      'yahoo_kgin_best',
                      #'facebook_kgtore_best'
                      ]
 
-movielens_best_models = ['movielens_best_models',
+movielens_best_models = ['movielens_noknowledge_best',
                          'movielens_kahfm_best',
                          'movielens_kgflex_best',
                          'movielens_kgin_best']
