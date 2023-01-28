ffbe6bf72c63e385f8ddf52eaa797b9a981cfb77
albertomancino <alberto.mancino94@gmail.com>
2023-01-28 17:13:50 +0100

    facebook best

--- best_models.py
+++ best_models.py
@@ -3,13 +3,7 @@ from elliot.run import run_experiment
 
 CONFIG_DIR = './config_files'
 
-best_models = [
-    'cf_facebook_best',
-    'kahfm_facebook_results',
-    'kgflex_facebook_results',
-    'kgin_facebook_results',
-    ''
-    ]
+best_models = ['facebook_best_models']
 
 for b in best_models:
     assert os.path.exists(os.path.join(CONFIG_DIR, b + '.yml'))
