c3877ec3daa695079fcd9d2741443e5ff17382ad
albertomancino <alberto.mancino94@gmail.com>
2023-01-28 18:50:55 +0100

    yahoo best

--- best_models.py
+++ best_models.py
@@ -3,11 +3,20 @@ from elliot.run import run_experiment
 
 CONFIG_DIR = './config_files'
 
-best_models = [#'facebook_best_models',
-               #'facebook_kahfm_best',
-               #'facebook_kgflex_best',
-               #'facebook_kgin_best',
-               'facebook_kgtore_best']
+facebook_best_models = ['facebook_best_models',
+                        'facebook_kahfm_best',
+                        'facebook_kgflex_best',
+                        'facebook_kgin_best',
+                        'facebook_kgtore_best']
+
+yahoo_best_models = ['yahoo_best_models',
+                     'yahoo_kahfm_best',
+                     'yahoo_kgflex_best',
+                     'yahoo_kgin_best',
+                     #'facebook_kgtore_best'
+                     ]
+
+best_models = yahoo_best_models
 
 for b in best_models:
     assert os.path.exists(os.path.join(CONFIG_DIR, b + '.yml'))
