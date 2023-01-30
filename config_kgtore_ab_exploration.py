aa6f9433ec16747102b8fadaf7f550366331777a
albertomancino <alberto.mancino94@gmail.com>
2023-01-30 18:49:26 +0100

    movielens

--- config_kgtore_ab_exploration.py
+++ config_kgtore_ab_exploration.py
@@ -28,7 +28,7 @@ KGTORE_CONFIG = """experiment:
       l_w: 0.00312734853526998
       alpha: {alpha}
       beta: {beta}
-      gamma: 0.054524737
+      gamma: {gamma}
       ind_edges: {ind_edges}
       epochs: 200
       factors: 64
--- start_experiments_kgtore_exploration.py
+++ start_experiments_kgtore_exploration.py
@@ -9,6 +9,7 @@ parser.add_argument('--npr', type=str, nargs='+', default=[40])
 parser.add_argument('--layer', type=str, nargs='+', default=[3])
 parser.add_argument('--alpha', type=str, nargs='+', default=[0.2, 0.4, 0.6, 0.8])
 parser.add_argument('--beta', type=str, nargs='+', default=[0, 0.1, 0.3, 0.5, 0.7, 0.9])
+parser.add_argument('--gamma', type=str, nargs='+', default=[0.5])
 parser.add_argument('--criterion', type=str, nargs='+', default=['entropy'])
 parser.add_argument('--gpu', type=int, default=0)
 args = parser.parse_args()
@@ -20,6 +21,7 @@ dataset = args.dataset
 alphas = args.alpha
 betas = args.beta
 gpu = args.gpu
+gammas = args.gamma
 
 print('Exploration of parameters:\n'
       f'npr: {nprs}\n'
@@ -27,34 +29,40 @@ print('Exploration of parameters:\n'
       f'layer: {layers}\n'
       f'dataset: {dataset}\n'
       f'alpha : {alphas}\n'
-      f'beta: {betas}\n')
+      f'beta: {betas}\n'
+      f'gamma: {gammas}\n'
+      )
 
 for d in dataset:
     for alpha in alphas:
         for beta in betas:
-            print(f'Starting training with '
-                  f'dataset: {d}\n'
-                  f'alpha: {alpha}\n'
-                  f'beta: {beta}')
+            for gamma in gammas:
+                print(f'Starting training with '
+                      f'dataset: {d}\n'
+                      f'alpha: {alpha}\n'
+                      f'beta: {beta}'
+                      f'gamma: {gammas}\n'
+                      )
 
-            assert d in ['facebook_book', 'movielens', 'yahoo_movies']
+                assert d in ['facebook_book', 'movielens', 'yahoo_movies']
 
-            ind_edges = {'facebook_book': 0.01,
-                           'movielens': 0.001,
-                           'yahoo_movies': 0.001}
+                ind_edges = {'facebook_book': 0.01,
+                               'movielens': 0.001,
+                               'yahoo_movies': 0.001}
 
-            config = KGTORE_CONFIG.format(dataset=d,
-                                          alpha=float(alpha),
-                                          beta=float(beta),
-                                          gpu=int(gpu),
-                                          ind_edges=float(ind_edges[d]))
-            config_dir = './config_files'
-            config_path = os.path.join(config_dir, 'runtime_conf.yml')
-            with open(config_path, 'w') as file:
-                file.write(config)
-            run_experiment(config_path)
+                config = KGTORE_CONFIG.format(dataset=d,
+                                              alpha=float(alpha),
+                                              beta=float(beta),
+                                              gamma=float(gamma),
+                                              gpu=int(gpu),
+                                              ind_edges=float(ind_edges[d]))
+                config_dir = './config_files'
+                config_path = os.path.join(config_dir, 'runtime_conf.yml')
+                with open(config_path, 'w') as file:
+                    file.write(config)
+                run_experiment(config_path)
 
-            print(f'Starting training with '
-                  f'dataset: {d}\n'
-                  f'alpha: {alpha}\n'
-                  f'beta: {beta}')
+                print(f'Starting training with '
+                      f'dataset: {d}\n'
+                      f'alpha: {alpha}\n'
+                      f'beta: {beta}')
