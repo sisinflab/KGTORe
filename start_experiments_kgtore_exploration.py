ec076bbd118bf10bc1f0d9e818e285f2e752a408
albertomancino <alberto.mancino94@gmail.com>
2023-01-30 19:46:24 +0100

    movielens

--- start_experiments_kgtore_exploration.py
+++ start_experiments_kgtore_exploration.py
@@ -9,7 +9,6 @@ parser.add_argument('--npr', type=str, nargs='+', default=[40])
 parser.add_argument('--layer', type=str, nargs='+', default=[3])
 parser.add_argument('--alpha', type=str, nargs='+', default=[0.2, 0.4, 0.6, 0.8])
 parser.add_argument('--beta', type=str, nargs='+', default=[0, 0.1, 0.3, 0.5, 0.7, 0.9])
-parser.add_argument('--gamma', type=str, nargs='+', default=[0.5])
 parser.add_argument('--criterion', type=str, nargs='+', default=['entropy'])
 parser.add_argument('--gpu', type=int, default=0)
 args = parser.parse_args()
@@ -21,7 +20,6 @@ dataset = args.dataset
 alphas = args.alpha
 betas = args.beta
 gpu = args.gpu
-gammas = args.gamma
 
 print('Exploration of parameters:\n'
       f'npr: {nprs}\n'
@@ -30,39 +28,62 @@ print('Exploration of parameters:\n'
       f'dataset: {dataset}\n'
       f'alpha : {alphas}\n'
       f'beta: {betas}\n'
-      f'gamma: {gammas}\n'
       )
 
 for d in dataset:
     for alpha in alphas:
         for beta in betas:
-            for gamma in gammas:
-                print(f'Starting training with '
-                      f'dataset: {d}\n'
-                      f'alpha: {alpha}\n'
-                      f'beta: {beta}'
-                      f'gamma: {gammas}\n'
-                      )
+            print(f'Starting training with '
+                  f'dataset: {d}\n'
+                  f'alpha: {alpha}\n'
+                  f'beta: {beta}'
+                  f'gamma: {gammas}\n'
+                  )
 
-                assert d in ['facebook_book', 'movielens', 'yahoo_movies']
+            assert d in ['facebook_book', 'movielens', 'yahoo_movies']
 
-                ind_edges = {'facebook_book': 0.01,
-                               'movielens': 0.001,
-                               'yahoo_movies': 0.001}
+            ind_edges = {'facebook_book': 0.01,
+                         'movielens': 0.00001,
+                         'yahoo_movies': 0.001}
 
-                config = KGTORE_CONFIG.format(dataset=d,
-                                              alpha=float(alpha),
-                                              beta=float(beta),
-                                              gamma=float(gamma),
-                                              gpu=int(gpu),
-                                              ind_edges=float(ind_edges[d]))
-                config_dir = './config_files'
-                config_path = os.path.join(config_dir, 'runtime_conf.yml')
-                with open(config_path, 'w') as file:
-                    file.write(config)
-                run_experiment(config_path)
+            lrs = {'facebook_book': 0.00041155060113429626,
+                   'yahoo_movies': 0.000664750387531533,
+                   'movielens': 0.00020519696043968285}
+            elrs = {'facebook_book': 0.00041155060113429626,
+                    'yahoo_movies': 0.00305727041704464,
+                    'movielens': 0.0014346784702323088}
+            l_ws = {'facebook_book': 0.00041155060113429626,
+                    'yahoo_movies': 0.00312734853526998,
+                    'movielens': 0.060707796238838284}
+            gammas = {'facebook_book': 0.00041155060113429626,
+                      'yahoo_movies': 0.054524737,
+                      'movielens': 0.9343878992507734}
 
-                print(f'Starting training with '
-                      f'dataset: {d}\n'
-                      f'alpha: {alpha}\n'
-                      f'beta: {beta}')
+            lr = lrs[d]
+            elr = elrs[d]
+            l_w = l_ws[d]
+            gamma = gammas[d]
+
+            if int(beta) == 1:
+                gamma = 0
+
+            config = KGTORE_CONFIG.format(dataset=d,
+                                          alpha=float(alpha),
+                                          beta=float(beta),
+                                          lr=lr,
+                                          elr=elr,
+                                          l_w=l_w,
+                                          gamma=gamma,
+                                          gpu=int(gpu),
+                                          ind_edges=float(ind_edges[d]))
+
+            config_dir = './config_files'
+            config_path = os.path.join(config_dir, 'runtime_conf.yml')
+            with open(config_path, 'w') as file:
+                file.write(config)
+            run_experiment(config_path)
+
+            print(f'Starting training with '
+                  f'dataset: {d}\n'
+                  f'alpha: {alpha}\n'
+                  f'beta: {beta}')
