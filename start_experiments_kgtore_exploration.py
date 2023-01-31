65b1d3b5ef58f4d61c00be6cfad9710765c23e48
albertomancino <alberto.mancino94@gmail.com>
2023-01-31 09:52:49 +0100

    movielens

--- start_experiments_kgtore_exploration.py
+++ start_experiments_kgtore_exploration.py
@@ -64,7 +64,7 @@ for d in dataset:
                 lr = float(lrs[d])
                 elr = float(elrs[d])
                 l_w = float(l_ws[d])
-                gamma = float(gammas)
+                gamma = float(gamma)
                 batch = int(batch_sizes[d])
 
                 print(f'Starting training with '
