2bd604f0401d5401066aa3c4441a24fd099b7a18
Salvatore Bufi <88724935+salvatore-bufi@users.noreply.github.com>
2023-04-19 21:04:33 +0200

    clustered metrics

--- config_metrics_template.py
+++ config_metrics_template.py
@@ -12,9 +12,9 @@ METRICS_TEMPLATE = """experiment:
     paired_ttest: True
     simple_metrics: [nDCGRendle2020, HR, Precision, Recall]
     complex_metrics:
-        - metric: clustered_nDCG
-        user_clustering_name: WarmColdUsers
-        user_clustering_file: ../data/{dataset}/users_deg.tsv
+      - metric: clustered_nDCG
+      user_clustering_name: WarmColdUsers
+      user_clustering_file: ../data/{dataset}/users_deg.tsv
   gpu: 0
   models:
     RecommendationFolder:  
