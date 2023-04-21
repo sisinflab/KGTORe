d3f311d9e1e96d3757955db9a4192e8f08e6d368
Salvatore Bufi <88724935+salvatore-bufi@users.noreply.github.com>
2023-04-21 10:24:16 +0200

    update for clustered

--- compute_metrics_on_recs.py
+++ compute_metrics_on_recs.py
@@ -3,18 +3,19 @@ from config_metrics_template import METRICS_TEMPLATE
 import os
 
 # RECS_FOLDER = os.path.abspath('./results/recs')
-RECS_FOLDER = os.path.abspath('./results/yahoo_movies/recs')
+# RECS_FOLDER = os.path.abspath('./results/yahoo_movies/recs')
+RECS_FOLDER = os.path.abspath('./results/recs')
 CONFIG_DIR = './config_files'
 assert os.path.exists(RECS_FOLDER)
 assert os.path.exists(CONFIG_DIR)
 
 recs_folder = RECS_FOLDER
-# datasets = ['facebook_book', 'yahoo_movies', 'movielens']
-datasets = ['yahoo_movies']
+datasets = ['facebook_book', 'yahoo_movies', 'movielens']
+# datasets = ['yahoo_movies']
 for dataset in datasets:
     print(f'Computing recs for {dataset}')
-    # recs_folder = os.path.join(RECS_FOLDER, dataset)
-    recs_folder = RECS_FOLDER
+    recs_folder = os.path.join(RECS_FOLDER, dataset)
+    # recs_folder = RECS_FOLDER
     assert os.path.exists(recs_folder)
 
     config = METRICS_TEMPLATE.format(dataset=dataset, recs=recs_folder)
