c18d7d197a716b37ef53db35100a0bb42dbde553
salvatore-bufi <bufi.salvatore.94@gmail.com>
2023-12-22 10:39:44 +0100

    for depth analysis

--- depth_anal.py
+++ depth_anal.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 path = './data/depth_analysis'
-dataset_name = 'yahoo'
+dataset_names = ['yahoo', 'facebook']
 criterion = 'entropy'
 nprs = [1, 2, 5, 10, 15, 20]
 
@@ -18,21 +18,23 @@ def load_dataset(path: str) -> pd.DataFrame:
     return df
 
 def depth_analysis(df_dict: dict, nprs: list) -> np.array:
-    data = pd.DataFrame(np.array([[int(npr), df_dict[npr]['depth'].mean(), df_dict[npr]['depth'].std()] for npr in nprs]))
-    data.columns = ['npr', 'mean_depth', 'std_depth']
+    data = pd.DataFrame(np.array([[int(npr), df_dict[npr]['depth'].mean(), df_dict[npr]['depth'].std(), np.max(df_dict[npr]['depth']), np.min(df_dict[npr]['depth'])] for npr in nprs]))
+    data.columns = ['npr', 'mean_depth', 'std_depth', 'max_depth', 'min_depth']
     data['npr'] = data['npr'].astype(int)
     return data
 
 
 
+results = dict()
+for dataset_name in dataset_names:
+    df_dict = dict()
+    for npr in nprs:
+        current_path = create_path(start_dir=path, dataset_name=dataset_name, criterion=criterion, npr=npr)
+        df_dict[npr] = load_dataset(current_path)
+    results[dataset_name] = depth_analysis(df_dict=df_dict, nprs=nprs)
 
-df_dict = dict()
-for npr in nprs:
-    current_path = create_path(start_dir=path, dataset_name=dataset_name, criterion=criterion, npr=npr)
-    df_dict[npr] = load_dataset(current_path)
-
-results = depth_analysis(df_dict=df_dict, nprs=nprs)
 print()
+
 
 
 
