71350f5874a2e9191d9649e50caa8c3b3136a6ee
salvatore-bufi <bufi.salvatore.94@gmail.com>
2023-12-22 16:17:36 +0100

    iron_graphs corrections

--- external/models/kgtore/KGTORE.py
+++ external/models/kgtore/KGTORE.py
@@ -10,8 +10,8 @@ from elliot.recommender import BaseRecommenderModel
 from elliot.recommender.base_recommender_model import init_charger
 from elliot.recommender.recommender_utils_mixin import RecMixin
 from .KGTOREModel import KGTOREModel
-# from .DecisionPaths import DecisionPaths
-from .DecisionPathsDepth import DecisionPaths
+from .DecisionPaths import DecisionPaths
+# from .DecisionPathsDepth import DecisionPaths
 from .LoadEdgeFeatures import LoadEdgeFeatures
 
 
