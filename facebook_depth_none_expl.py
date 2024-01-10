d67c45a6e3ccd415ea684183532c254c425d56c1
salvatore-bufi <bufi.salvatore.94@gmail.com>
2024-01-10 11:10:26 +0100

    exploration for unlimited depth

---
+++ facebook_depth_none_expl.py
@@ -0,0 +1,22 @@
+from elliot.run import run_experiment
+from config_fb_depth_none import TEMPLATE
+import os
+
+CONFIG_DIR = './config_files'
+
+assert os.path.exists(CONFIG_DIR)
+
+# datasets = ['facebook_book', 'yahoo_movies', 'movielens']
+# alphas = [str(i) for i in [0.4]]
+a = str(0.6)
+
+nprs = [str(i) for i in [1, 2, 5, 10, 20]]
+
+dataset = 'facebook_book'
+for npr in nprs:
+    config = TEMPLATE.format(dataset, dataset, dataset, dataset, alpha=a,  npr=npr)
+    name = dataset + '_iron_tore' + '_' + 'a' + a + '_' + npr + '_' + 'None' + '.yml'
+    config_path = os.path.join(CONFIG_DIR, name)
+    with open(config_path, 'w') as file:
+        file.write(config)
+    run_experiment(config_path)
