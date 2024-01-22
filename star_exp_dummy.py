a5e8cade6ae03e68b67138fae5416255b2993f4b
salvatore-bufi <bufi.salvatore.94@gmail.com>
2024-01-22 13:22:25 +0100

    run best conf

--- star_exp_dummy.py
+++ star_exp_dummy.py
@@ -1,4 +1,6 @@
 from elliot.run import run_experiment
 
+files = ["config_files/facebook_best_kguf.yml", "config_files/movielens_best_kguf.yml"]
 # run_experiment(f"config_files/yahoo_seed_expl.yml")
-run_experiment(f"config_files/yahoo_prova_seed.yml")
+for file in files:
+    run_experiment(file)
