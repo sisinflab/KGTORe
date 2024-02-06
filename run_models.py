427c38773f48a111026e84b039eb0567ff63f835
salvatore-bufi <bufi.salvatore.94@gmail.com>
2024-02-06 11:42:54 +0100

    model update to support a and beta, runtime config file exp

--- run_models.py
+++ run_models.py
@@ -9,7 +9,7 @@ CONFIG_DIR = './config_files'
 assert os.path.exists(CONFIG_DIR)
 
 # Preprocessing
-yahoo_movies_preprocessing.run(data_folder=yahoo_movies_folder)
+# yahoo_movies_preprocessing.run(data_folder=yahoo_movies_folder)
 
 # alpha beta parameters:
 alphas = [str(i) for i in [0, 0.2, 0.4, 0.6, 0.8, 1]]
@@ -22,9 +22,10 @@ dataset = 'yahoo_movies'
 for aggr in models_aggr:
     for a in alphas:
         for b in betas:
-            config = yahoo_template.format(dataset, dataset, dataset, dataset, alpha=a, beta=b, aggr=aggr)
-            name = dataset + '_kgtore_' + aggr + '_' + 'a_' + a + 'b_' + b + '.yml'
-            config_path = os.path.join(CONFIG_DIR, name)
-            with open(config_path, 'w') as file:
-                file.write(config)
-            run_experiment(config_path)
+            if (a != '0') or (b != '0'):
+                config = yahoo_template.format(dataset, dataset, dataset, dataset, alpha=a, beta=b, aggr=aggr)
+                name = dataset + '_kgtore_' + aggr + '_' + 'a_' + a + 'b_' + b + '.yml'
+                config_path = os.path.join(CONFIG_DIR, name)
+                with open(config_path, 'w') as file:
+                    file.write(config)
+                run_experiment(config_path)
